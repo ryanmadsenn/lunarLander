865bfc2ed5aa606068e099289c973c0f66946218
ryanmadsenn <97546348+ryanmadsenn@users.noreply.github.com>
2022-10-08 11:02:56 -0600

    Updated lander class

--- lander.cpp
+++ lander.cpp
@@ -7,11 +7,46 @@
 
 #include <stdio.h>
 #include <iostream>
+#include "point.h"
 
 using namespace std;
 
 class lander {
-    int fuel;
+private:
+    Point position;
+    double fuel = 5000.0;
+    double directionRadians;
+    double velocityX;
+    double velocityY;
+    double totalVelocity;
+    double thrust;
+    double weight;
+
+public:
+    void Thrust() {}
+    void rotate() {}
+    
+    Point getPostion() {
+        return this->position;
+    }
+    
+    double getDirectionRadians() {
+        return this->directionRadians;
+    }
+    
+    double getVelocityX() {
+        return this->velocityX;
+    }
+    
+    double getVelocityY() {
+        return this->velocityY;
+    }
+    
+    double getTotalVelocity() {
+        return this->velocityY;
+    }
+
+    
     
     
     
