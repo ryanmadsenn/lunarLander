a988acc55f4491b7b9a4fd7b43cdaad4d16f0abf
ryanmadsenn <97546348+ryanmadsenn@users.noreply.github.com>
2022-10-08 10:40:53 -0600

    Added files

---
+++ lander.cpp
@@ -0,0 +1,18 @@
+//
+//  lander.cpp
+//  Lab03
+//
+//  Created by Ryan Madsen on 10/7/22.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class lander {
+    int fuel;
+    
+    
+    
+};
